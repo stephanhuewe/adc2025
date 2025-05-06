286639945ccab2e39d78d3f08304008874893ab8
stephanhuewe <sh@huewe.info>
2025-05-06 22:11:29 +0200

    NuGet HELL

--- ADC2025_Samples/VectorDemo/Program.cs
+++ ADC2025_Samples/VectorDemo/Program.cs
@@ -1,12 +1,12 @@
-﻿using System.ClientModel;
-using System.Linq.Expressions;
+﻿
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.VectorData;
-using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.InMemory;
-using OpenAI;
-using OpenAI.Embeddings;
+using OpenAI.VectorStores;
 
+
+
+#pragma warning disable SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 public class Movie
 {
@@ -16,28 +16,34 @@ public class Movie
     [VectorStoreRecordData]
     public string Title { get; set; }
 
-    [VectorStoreRecordVector(Dimensions: 384, DistanceFunction = DistanceFunction.CosineSimilarity)]
+    [VectorStoreRecordVector(Dimensions: 384, DistanceFunction.CosineSimilarity)]
     public ReadOnlyMemory<float> Vector { get; set; }
 }
 
 public class Demo
 {
-    public static async Task Run()
+    public static async Task Main()
     {
-        Movie superHeroMovie = new Movie() { MyKey = 1, Title = "ADC Hero 2025" };
+        Movie superHeroMovie = new Movie() { MyKey = 1, Title = "ADC Comedy 2025" };
         Movie superHeroMovie2 = new Movie() { MyKey = 2, Title = "ADC SuperHero 2025" };
+        Movie superHeroMovie3 = new Movie() { MyKey = 2, Title = "ADC Batman 2025" };
 
         List<Movie> movies = new List<Movie>
         {
             superHeroMovie,
-            superHeroMovie2
+            superHeroMovie2,
+            superHeroMovie3
         };
 
+       
+
         // Vector
+
         var store = new InMemoryVectorStore();
         var collection = store.GetCollection<int, Movie>("movies");
-
-        var generator = new OpenAIEmbeddingGenerator("gpt-4o-mini");
+        var generator =  new OpenAIEmbeddingGenerator(new OpenAI.OpenAIClient(Environment.GetEnvironmentVariable("OpenAI:ApiKey")), "text-embedding-3-large");
+        
+        await collection.CreateCollectionIfNotExistsAsync();
 
 
         foreach (Movie movie in movies)
@@ -46,9 +52,15 @@ public class Demo
             await collection.UpsertAsync(movie);
         }
 
+        var searchOptions = new VectorSearchOptions()
+        {
+            Top = 1,
+            VectorPropertyName = "Vector",
+        };
+
         var search = "superhero movie";
         var vector = await generator.GenerateEmbeddingVectorAsync(search);
-        var results = await collection.VectorizedSearchAsync(vector, new VectorSearchOptions { Top = 1, VectorPropertyName = "Vector" });
+        var results = await collection.VectorizedSearchAsync(vector, searchOptions);
 
         await foreach (var result in results.Results)
         {
